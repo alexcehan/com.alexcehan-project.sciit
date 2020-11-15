c10aca78ad2d627736919de350386d0277abcdd3
alexcehan <alexandru.cehan@gmail.com>
2020-11-15 17:58:47 +0200

    Initial Commit

--- src/main/java/week1/homeworks/NumberOfDaysInMonth.java
+++ src/main/java/week1/homeworks/NumberOfDaysInMonth.java
@@ -1,4 +1,12 @@
 package week1.homeworks;
 
+import java.util.Scanner;
+
 public class NumberOfDaysInMonth {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
--- src/main/java/week2/homeworks/ConvertTemperature.java
+++ src/main/java/week2/homeworks/ConvertTemperature.java
@@ -1,4 +1,27 @@
 package week2.homeworks;
 
+import java.util.Scanner;
+
 public class ConvertTemperature {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double temperature = 0.0;
+
+        System.out.println("Enter the value in Fahrenheit to convert");
+
+        boolean flag = true;
+
+        while(flag) {
+            if(scanner.hasNextDouble()) {
+                temperature = scanner.nextDouble();
+                flag = false;
+            } else {
+                scanner.nextLine();
+                System.out.println("Please enter a valid value!");
+            }
+        }
+
+        System.out.println(temperature + " degree Fahrenheit is equal to " + ((temperature - 32)/1.8) + " in Celsius");
+    }
 }
