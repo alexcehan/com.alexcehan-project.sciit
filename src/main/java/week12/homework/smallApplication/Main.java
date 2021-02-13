12b7808a5ed6e1691bd9afe4e7b9f404999d53c6
alexcehan <alexandru.cehan@gmail.com>
2021-02-13 23:19:01 +0200

    Initial Commit

--- src/main/java/week12/homework/smallApplication/Main.java
+++ src/main/java/week12/homework/smallApplication/Main.java
@@ -6,6 +6,31 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**By using streams and lamba expressions, implement a small application which:
+
+ 1.load from a file a structure like
+
+ first name, last name, date of birth
+
+ 2. write back another file containing only
+
+ first name, last name
+
+ ordered alphabetically for all the of all matches which have the birthday on a month indicated.
+
+
+
+ Application takes 3 params:
+
+ 1. input filename
+
+ 2. target month (1-12)
+
+ 3. output file name
+
+
+ */
+
 public class Main {
     public static void main(String[] args)  {
 
