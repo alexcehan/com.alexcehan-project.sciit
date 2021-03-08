259b6fd29415924c2df48fecf9305f65a9e58d54
Alex <alexandru.cehan@gmail.com>
2021-03-08 13:57:58 +0200

    Week 14 homework first commit

--- src/main/java/week13/homework/FestivalApp.java
+++ src/main/java/week13/homework/FestivalApp.java
@@ -3,11 +3,11 @@ package week13.homework;
 public class FestivalApp {
 
     public static void main(String[] args) {
-      Thread keepGates = new KeepGatesOpen();
+      Thread keepGatesOpen = new KeepGatesOpen();
       Thread stats = new FestivalStatisticsThread();
 
         stats.start();
-        keepGates.start();
+        keepGatesOpen.start();
 
 
 
